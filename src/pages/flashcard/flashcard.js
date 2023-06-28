11b7a5d51f18e27934c4d26a6f74638391127206
pujaribalaji <pujaribalaji152@gmail.com>
2023-06-28 23:27:18 +0530

    flashcards

--- src/pages/flashcard/flashcard.js
+++ src/pages/flashcard/flashcard.js
@@ -1,30 +1,50 @@
 import React from 'react';
 // import rectangle1 from 'C:/Users/Balaji/Desktop/reactapp/reactapp/src/components/asserts/Rectangle1.png'/
-import rectangle1 from '../../components/asserts/Rectangle1.png' ;
-import rectangle2 from '../../components/asserts/Rectangle2.png' ;
-import rectangle3 from '../../components/asserts/Rectangle3.png' ;
+import rectangle1 from '../../components/asserts/Rectangle1.png';
+import rectangle2 from '../../components/asserts/Rectangle2.png';
+import rectangle3 from '../../components/asserts/Rectangle3.png';
 
 import './flashcard.css'
 import 'bootstrap/dist/css/bootstrap.css';
 
- function  Flashcard() {
-  return (
-    <>
-    <div className="f1">
-      Recent cards
-    </div>
-    <div class="container">
-        <div class="img0"></div>
-        <div class="img1"></div>
-        <div class="img2"></div>
-    </div>
-    <p className='f2'>Lorem Ipsum</p>
-    </>
-    
-  )
+function Flashcard() {
+    return (
+        <>
+            <div className="f1">
+                Recent cards
+            </div>
+            <div className='row'>
+                <div className='container-1'>
+
+                    <div className='col c1'>
+                        <a className='img-1'> <img src={rectangle1} /></a>
+                        <p>Lorem Ipsum</p>
+                        <p className='f2'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                    <div className='col'>
+                        <a className='img-2'> <img src={rectangle2} /></a>
+                        <p>Lorem Ipsum </p>
+                        <p className='f2'> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                    <div className='col'>
+                        <a className='img-3'> <img src={rectangle3} /></a>
+                        <p>Lorem Ipsum</p>
+                        <p className='f2'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+
+
+                </div>
+
+            </div>
+
+
+
+        </>
+
+    )
 }
 
-export default  Flashcard;
+export default Flashcard;
 
 
 
