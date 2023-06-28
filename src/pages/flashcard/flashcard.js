139d2def30c2c2e47a2caa4e49c2de5810b208a3
pujaribalaji <pujaribalaji152@gmail.com>
2023-06-28 09:10:24 +0530

    Add Rectangle3 and some changes in flashcard.css and js

--- src/pages/flashcard/flashcard.js
+++ src/pages/flashcard/flashcard.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import rectangle1 from 'C:/Users/Balaji/Desktop/reactapp/reactapp/src/components/asserts/Rectangle1.png'/
 import rectangle1 from '../../components/asserts/Rectangle1.png' ;
 import rectangle2 from '../../components/asserts/Rectangle2.png' ;
+import rectangle3 from '../../components/asserts/Rectangle3.png' ;
 
 import './flashcard.css'
 import 'bootstrap/dist/css/bootstrap.css';
@@ -17,6 +18,7 @@ import 'bootstrap/dist/css/bootstrap.css';
         <div class="img1"></div>
         <div class="img2"></div>
     </div>
+    <p className='f2'>Lorem Ipsum</p>
     </>
     
   )
