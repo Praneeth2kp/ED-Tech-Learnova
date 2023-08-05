9abd381f432a6e31d32607f4ebf12e88dcb3e0ed
praneeth2kp <praneeth2kp@gmail.com>
2023-08-05 18:41:46 +0530

    praneeth test commit

--- src/pages/quizzes/quizzes.js
+++ src/pages/quizzes/quizzes.js
@@ -1,7 +1,7 @@
 import React from "react";
-
 import 'bootstrap/dist/css/bootstrap.css';
 import "./quizzes.css";
+import brainprogress from '../../components/asserts/brain-progress.svg'
 
 function Quizzes() {
     return (
@@ -12,14 +12,20 @@ function Quizzes() {
             <div className="row">
                 <div className="ybox">
                     <div className="row">
-                        <div className="col-10">
+                        <div className="col-9">
                             <h5 className="hfive">Lorem Ipsum</h5>
                             <p className="para">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                                 when an unknown printer took a galley of type and scrambled it to make a type
                                 specimen book.</p>
                         </div>
-                        <div className="col-2">
+                        <div className="col-3">
+                            <div className="cpro" data-progress="50">
+                            <img src={brainprogress} alt="Image" />
+                            <div className="proimg">
+
+                            </div>
+                            </div> 
                         </div>
                     </div>
                 </div>
